841f225bd8715e0b6f3abeaa545e4803a13888a7
amaurel <alexandre.maurel@gmail.com>
2017-11-14 14:29:14 +0100

    new factors

--- main.R
+++ main.R
@@ -2,8 +2,8 @@ library(tercen)
 library(dplyr)
 
 (ctx = tercenCtx())  %>% 
-  select(.values, .cindex, .rindex) %>% 
-  group_by(.cindex, .rindex) %>%
-  summarise(mean = mean(.values)) %>%
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(mean = mean(.y)) %>%
   ctx$addNamespace() %>%
   ctx$save()
