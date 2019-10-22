7b0f5a0e6b25e5a4f64242ff2baca2b3e65a082a
Alexandre Maurel <alexandre.maurel@gmail.com>
2019-10-22 17:06:35 +0000

    revert as.matrix

--- main.R
+++ main.R
@@ -1,10 +1,9 @@
 library(tercen)
 library(dplyr)
 
-mean.matrix = (ctx = tercenCtx()) %>% as.matrix(fill=NaN) 
-
-data = data.frame(.ri = as.vector(row(mean.matrix)-1),
-                  .ci = as.vector(col(mean.matrix)-1),
-                  mean = as.vector(mean.matrix)) %>%
+(ctx = tercenCtx())  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(mean = mean(.y)) %>%
   ctx$addNamespace() %>%
   ctx$save()
