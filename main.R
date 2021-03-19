2b2ca2994ff46b5f6cd513a650cdc2d569ffaa8d
Alexandre Maurel <aalexandre.maurel@gmail.com>
2021-03-19 10:32:50 +0000

    ctx

--- main.R
+++ main.R
@@ -1,7 +1,11 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
+# options("tercen.workflowId"= "2e6fffab1bcd8ff472773c007d0e4c9d")
+# options("tercen.stepId"= "ebda5081-aae4-45fc-b91b-f7b4b19d402a")
+ctx = tercenCtx()
+
+ctx  %>% 
   select(.y, .ci, .ri) %>% 
   group_by(.ci, .ri) %>%
   summarise(mean = mean(.y)) %>%
