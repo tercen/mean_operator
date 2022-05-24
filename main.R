88c1f4e81a315600e70431a034f6ea49a751dccf
Alexandre Maurel <alexandre.maurel@gmail.com>
2022-05-24 11:12:21 +0000

    use data.table

--- main.R
+++ main.R
@@ -1,11 +1,16 @@
+library(tercenApi)
 library(tercen)
-library(dplyr)
+library(data.table)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
 
 ctx = tercenCtx()
 
 ctx  %>% 
   select(.y, .ci, .ri) %>% 
+  lazy_dt() %>%
   group_by(.ci, .ri) %>%
   summarise(mean = mean(.y)) %>%
+  as_tibble() %>%
   ctx$addNamespace() %>%
   ctx$save()
