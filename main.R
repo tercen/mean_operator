ca940dd8e771d59932ad95ef09c9fea118ecb975
agouy <contact@agouy.com>
2023-09-20 07:45:03 +0000

    Added optional handling of x axis

--- main.R
+++ main.R
@@ -1,16 +1,33 @@
-library(tercenApi)
-library(tercen)
-library(data.table)
-library(dtplyr)
-library(dplyr, warn.conflicts = FALSE)
+suppressPackageStartupMessages({
+  library(tercenApi)
+  library(tercen)
+  library(data.table)
+  library(dtplyr)
+  library(dplyr, warn.conflicts = FALSE)
+})
 
 ctx = tercenCtx()
 
-ctx  %>% 
-  select(.y, .ci, .ri) %>% 
-  lazy_dt() %>%
-  group_by(.ci, .ri) %>%
-  summarise(value = mean(.y)) %>%
-  as_tibble() %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+if(ctx$hasNumericXAxis) {
+  
+  ctx  %>% 
+    select(.y, .x, .ci, .ri) %>% 
+    lazy_dt() %>%
+    group_by(.ci, .ri) %>%
+    summarise(mean_y = mean(.y), mean_x = mean(.x)) %>%
+    as_tibble() %>%
+    ctx$addNamespace() %>%
+    ctx$save()
+  
+} else {
+  
+  ctx  %>% 
+    select(.y, .ci, .ri) %>% 
+    lazy_dt() %>%
+    group_by(.ci, .ri) %>%
+    summarise(value = mean(.y)) %>%
+    as_tibble() %>%
+    ctx$addNamespace() %>%
+    ctx$save()
+  
+}
